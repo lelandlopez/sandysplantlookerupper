8a534fbfa355393c2d8b9716a19b23a9318ed3fe
Leland Lopez <lelandlopez@gmail.com>
2018-10-19 16:11:27 -1000

    have node connect to postgres, node wait for postgres to start

--- web/server.js
+++ web/server.js
@@ -1,15 +1,28 @@
 'use strict';
 
 const express = require('express');
+const {Pool, Client} = require('pg');
 
 // Constants
-const PORT = 8080;
+const PORT = 5432;
 const HOST = '0.0.0.0';
+const USER = 'user';
+const DATABASE = 'db';
+const PASSWORD = 'pass';
+
+const client = new Pool({
+  user: USER,
+  host: 'db',
+  database: DATABASE,
+  password: PASSWORD,
+  port: PORT,
+})
+client.connect();
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-
+client.connect()
   res.send('Hello world asdfasdf\n');
 });
 
