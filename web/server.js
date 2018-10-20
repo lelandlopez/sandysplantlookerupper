b599df6d549ea78da541261f3db467f3fbe1e62f
Leland Lopez <lelandlopez@gmail.com>
2018-10-19 17:15:20 -1000

    async's in express

--- web/server.js
+++ web/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const {Pool, Client} = require('pg');
 
 // Constants
-const INTERNAL_PORT = 8080;
+const INTERNAL_PORT = 8081;
 const EXTERNAL_PORT = 49160;
 const POSTGRES_PORT = 5432;
 const HOST = '0.0.0.0';
@@ -21,12 +21,31 @@ const client = new Pool({
 })
 client.connect();
 
+const asyncMiddleware = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next))
+    .catch(next);
+};
+
 // App
 const app = express();
-app.get('/', (req, res) => {
-client.connect()
-  res.send('Hello world asdfasdf\n');
-});
+app.get('/asdfasdf', asyncMiddleware(async (req, res, next) => {
+  res.json(await tempFunc());
+}));
+
+async function tempFunc() {
+  console.log('asdf');
+  const query = {
+    text: 'SELECT * FROM asdfasdf',
+    values: [],
+  }
+
+  // promise
+  let x =  await client.query(query)
+    .then(docs => {return docs})
+    .catch(e => {return e.stack});
+  console.log(x);
+  return x;
+}
 
 app.listen(INTERNAL_PORT, HOST);
 console.log(`Running on http://${HOST}:${EXTERNAL_PORT}`);
