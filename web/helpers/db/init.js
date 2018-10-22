0fd1b7ed901f2c13efbd2d38d592da894ff4fd99
Leland Lopez <lelandlopez@gmail.com>
2018-10-21 21:02:53 -1000

    cleaning up

--- web/helpers/db/init.js
+++ web/helpers/db/init.js
@@ -1,10 +1,13 @@
 const {Pool, Client} = require('pg');
 const tableInitStrings = require('./initTables.json')
+const multer = require('multer')
 
 const USER = 'user';
 const DATABASE = 'db';
 const PASSWORD = 'pass';
 const POSTGRES_PORT = 5432;
+
+const imageUploadFolder = 'public/images/uploads';
 
 const dbCreds = {
   user: USER,
@@ -29,9 +32,20 @@ async function initTables() {
   }
 }
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, imageUploadFolder)
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+const upload = multer({storage: storage});
+
 initTables();
 
 module.exports = {
   client,
-  queryWrapper
+  queryWrapper,
+  upload
 }
--- web/server.js
+++ web/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const {client, queryWrapper} = require('./helpers/db/init.js')
+const {client, queryWrapper, upload} = require('./helpers/db/init.js')
 const {asyncMiddleware} = require('./helpers/db/functions.js')
 
 // Constants
@@ -10,15 +10,6 @@ const INTERNAL_PORT = 8081;
 const EXTERNAL_PORT = 49160;
 const HOST = '0.0.0.0';
 
-let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/images/uploads')
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
-    }
-});
-var upload = multer({storage: storage});
 
 const app = express();
 app.use(bodyParser.json());
@@ -28,33 +19,30 @@ app.get('/asdfasdf', asyncMiddleware(async (req, res, next) => {
   res.json(await tempFunc());
 }));
 
-app.post('/uploadImage', upload.single('avatar'), (req, res) => {
-  if (!req.file) {
-    console.log("No file received");
-    return res.send({
-      success: false
-    });
-
-  } else {
-    console.log('file received');
-    const host = req.host;
-    const filePath = req.protocol + "://" + host + '/' + req.file.path;
-    console.log(filePath);
-    return res.send({
-      success: true
-    })
-
-  }
+app.post('/uploadImage', upload.single('image'), async (req, res) => {
+  res.json(await uploadImage(req));
 });
+
+
+app.listen(INTERNAL_PORT, HOST);
+console.log(`Running on http://${HOST}:${EXTERNAL_PORT}`);
+
+async function uploadImage() {
+  if (!req.file) {
+    return {
+      success: false
+    };
+  } else {
+    return {
+      success: true
+    };
+  }
+}
 
 async function tempFunc() {
   const query = {
     text: "SELECT * FROM pg_catalog.pg_tables WHERE schemaname = 'public'",
     values: [],
   }
-
   return await queryWrapper(query);
 }
-
-app.listen(INTERNAL_PORT, HOST);
-console.log(`Running on http://${HOST}:${EXTERNAL_PORT}`);
